e53255039b7c9b3d4920ab7a4dd5158f7453f5a4
abhishekjha7798 <akjdps@gmail.com>
2025-10-31 21:11:26 +0530

    Add segmented sieve algorithm to Sieve.java

--- snippets/Sieve.java
+++ snippets/Sieve.java
@@ -15,5 +15,38 @@ public class Sieve {
         }
         return isPrime;
     }
-}
 
+    // Segmented Sieve - Find all primes in range [left, right]
+    // Useful when the range is large but the difference is small
+    static boolean[] segmentedSieve(long left, long right) {
+        long limit = (long) Math.sqrt(right) + 1;
+        boolean[] basePrimes = sieve((int) limit);
+
+        boolean[] isPrime = new boolean[(int)(right - left + 1)];
+        for (int i = 0; i < isPrime.length; i++) {
+            isPrime[i] = true;
+        }
+
+        // Handle special case for 0 and 1
+        if (left <= 1) {
+            for (long i = left; i <= Math.min(right, 1); i++) {
+                isPrime[(int)(i - left)] = false;
+            }
+        }
+
+        // Mark multiples of primes in basePrimes
+        for (int i = 2; i <= limit; i++) {
+            if (basePrimes[i]) {
+                // Find first multiple of i in range [left, right]
+                long firstMultiple = ((left + i - 1) / i) * i;
+                if (firstMultiple == i) firstMultiple += i;
+
+                // Mark all multiples as composite
+                for (long j = firstMultiple; j <= right; j += i) {
+                    isPrime[(int)(j - left)] = false;
+                }
+            }
+        }
+        return isPrime;
+    }
+}
